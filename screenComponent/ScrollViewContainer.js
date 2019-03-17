6431c03ecb7bf0483fc2ad2ef83d21b6060a04bd
user <user@users-MacBook-Pro.local>
2019-03-17 11:55:26 +1100

    images added to horizontal scroll view

--- screenComponent/ScrollViewContainer.js
+++ screenComponent/ScrollViewContainer.js
@@ -2,20 +2,28 @@ import React from 'react';
 //import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { 
-    View,ScrollView,StyleSheet
+    View,ScrollView,StyleSheet,Image
 } from 'react-native';
 
 const ScrollViewContainer=()=>{
 return(
-        <ScrollView style={styles.SliderContainer} horizontal={true}>    
+        <ScrollView style={styles.SliderContainer} horizontal={true}
+            pagingEnabled={true}
+            scrollEventThrottle={10}
+            showsHorizontalScrollIndicator={false}
+        >    
             <View>
-
+                <Image source={require('../assets/download.png')} style={{borderColor:'black'}}/>
             </View>
+                <Image source={require('../assets/download.png')}/>
             <View>
+                <Image source={require('../assets/download.png')}/>
             </View>
+                <Image source={require('../assets/download.png')}/>
             <View>
-
+                <Image source={require('../assets/download.png')}/>
             </View>
+        
         </ScrollView>
 )
 }
